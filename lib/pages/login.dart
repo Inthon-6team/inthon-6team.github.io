1ee5b35b2512e0a7ff26a97bf217e468c7d597af
jjjh02 <jinnyshur@korea.ac.kr>
2023-11-25 23:54:18 +0900

    login 수정

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -114,7 +114,16 @@ class _LoginState extends State<Login> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
+                padding: EdgeInsets.fromLTRB(160, 10, 0, 0),
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    color: Color(0xFFA6633C),
+                    decoration: TextDecoration.underline,
+                  ),
+                )),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
               child: GestureDetector(
                 onTap: () {
                   Navigator.push(
@@ -147,7 +156,9 @@ class _LoginState extends State<Login> {
                     color: Color(0xFFD9AE89),
                     borderRadius: BorderRadius.circular(10.0)),
                 child: Row(children: [
-                  //ImageData(IconsPath.googlelogo, width: 20, height: 20),
+                  SizedBox(width: 45.0),
+                  ImageData(IconsPath.googlelogo, width: 20, height: 20),
+                  SizedBox(width: 10.0),
                   Text(
                     'Sign In with Google',
                     style: TextStyle(
@@ -157,6 +168,27 @@ class _LoginState extends State<Login> {
                   ),
                 ]),
               ),
+            ),
+            Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.fromLTRB(70, 10, 0, 0),
+                    child: Text(
+                      'Don\'t have an acount?',
+                      style: TextStyle(
+                        color: Color(0xFF9F9F9F),
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                    child: Text(
+                      'Sign Up!',
+                      style: TextStyle(
+                        color: Color(0xFFA6633C),
+                        decoration: TextDecoration.underline,
+                      ),
+                    )),
+              ],
             ),
           ],
         ),
