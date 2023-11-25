bd027f17de1a8e881c929913e60f01f95ee9cf3c
jjjh02 <jinnyshur@korea.ac.kr>
2023-11-26 04:53:17 +0900

    navigation bar 수정

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -78,6 +78,7 @@ class _HomeState extends State<Home> {
         child: SingleChildScrollView(
           child: Container(
             height: 550,
+            color: Colors.white,
             //padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 16.0),
             child: GridView.builder(
               shrinkWrap: true,
