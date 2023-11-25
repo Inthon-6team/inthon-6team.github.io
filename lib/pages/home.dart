60cdcbe95ba9634d6883d6f58bd7a3e2be13ff85
hhjeee <yoonhj0315@gmail.com>
2023-11-25 20:43:23 +0900

    메세지 수정 기능

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,7 +4,26 @@ import 'package:inthon_frontend/pages/notice.dart';
 import 'package:inthon_frontend/components/profile.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  //const Home({super.key});
+
+  final List<Map<String, String>> profileData = [
+    {
+      'statusText': "우리 가족 사랑해 ~",
+      'imagePath': 'assets/images/family2.png',
+    },
+    {
+      'statusText': "건 강 조 심",
+      'imagePath': 'assets/images/family2.png',
+    },
+    {
+      'statusText': "<시험기간> 바빠서 연락 잘 못 받음ㅜ",
+      'imagePath': 'assets/images/family1.png',
+    },
+    {
+      'statusText': "해커톤 나가서 24시간동안 연락 안돼용",
+      'imagePath': 'assets/images/family2.png',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,23 +49,28 @@ class Home extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            height: 550,
-            //padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 16.0),
-            child: GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, //그리드 열 수
-                crossAxisSpacing: 0, //가로 간격
-                mainAxisSpacing: 50.0, // 세로 간격
-                childAspectRatio: 0.8,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              height: 550,
+              //padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 16.0),
+              child: GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, //그리드 열 수
+                  crossAxisSpacing: 0, //가로 간격
+                  mainAxisSpacing: 50.0, // 세로 간격
+                  childAspectRatio: 0.8,
+                ),
+                itemCount: 4, //itemDataList.length,
+                itemBuilder: (context, index) {
+                  return Profile(
+                    statusText: profileData[index]['statusText']!,
+                    imagePath: profileData[index]['imagePath']!,
+                  );
+                },
               ),
-              itemCount: 4, //itemDataList.length,
-              itemBuilder: (context, index) {
-                return Profile();
-              },
             ),
           ),
         ),
