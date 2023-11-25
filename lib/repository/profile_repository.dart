02b210842f24927479290b4d638694c848ac1fbf
woog2roid <woog2roid@gmail.com>
2023-11-26 04:20:35 +0900

    feat(dio): UpdateStatusText

--- lib/repository/profile_repository.dart
+++ lib/repository/profile_repository.dart
@@ -36,3 +36,47 @@ Future<List<Map<String, String>>> fetchProfileData() async {
     rethrow;
   }
 }
+
+/*
+ 귀정: 이거 return 값으로 수정된 게 돌아옴 ㅇㅇ,,,
+ 수정된 거 받아가지고, 그걸로 바로 화면에 띄워주면 될 듯
+ (수정된거 보여주면 될 듯 ㅇㅇ)
+*/
+Future<Map<String, String>> updateStatusText(text) async {
+  var dio = Dio();
+  var url = 'https://hello-t2pqd7uv4q-uc.a.run.app/user';
+
+  try {
+    final authHeaders = await getAuthHeader();
+    // Response response = await _dio.post('/private-post',
+    //     data: data, options: Options(headers: authHeaders));
+
+    var response = await dio.patch(
+      url,
+      data: {
+        'introduction': text,
+      },
+      options: Options(headers: authHeaders)
+    );
+
+    if (response.statusCode == 200) {
+      print("Response data: ${response.data}");
+      Map<String, String> data = response.data;
+      return {
+        'nickName': data['name'] as String,
+        'statusText': data['introduction'] as String,
+        'imagePath': data['profile_image'] as String,
+      };
+    } else {
+      throw Exception('Failed to load profiles');
+    }
+  } catch (e) {
+    print('Request URL: $url');
+    if (e is DioError) {
+      print('DioError: ${e.response?.statusCode} ${e.response?.data}');
+    } else {
+      print('Error occurred: $e');
+    }
+    rethrow;
+  }
+}
