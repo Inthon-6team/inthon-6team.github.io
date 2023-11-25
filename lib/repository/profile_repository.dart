900d80faa5d5a8381a37911d3281cbc66663a7d7
woog2roid <woog2roid@gmail.com>
2023-11-26 07:49:18 +0900

    feat: nickname 수정 repository 생성

--- lib/repository/profile_repository.dart
+++ lib/repository/profile_repository.dart
@@ -118,3 +118,42 @@ Future<Map<String, dynamic>> fetchMyProfile() async {
     rethrow;
   }
 }
+
+Future<Map<String, dynamic>> updateName(name) async {
+  var dio = Dio();
+  var url = 'https://hello-t2pqd7uv4q-uc.a.run.app/user';
+
+  try {
+    final authHeaders = await getAuthHeader();
+    // Response response = await _dio.post('/private-post',
+    //     data: data, options: Options(headers: authHeaders));
+
+    var response = await dio.patch(
+      url,
+      data: {
+        'name': name,
+      },
+      options: Options(headers: authHeaders)
+    );
+
+    if (response.statusCode == 200) {
+      print("Response data: ${response.data}");
+      Map<String, dynamic> data = response.data;
+      return {
+        'nickName': data['name'] as String,
+        'statusText': data['introduction'] as String,
+        'imagePath': data['profile_image'] as String,
+      };
+    } else {
+      throw Exception('Failed to load profiles');
+    }
+  } catch (e) {
+    print('Request URL: $url');
+    if (e is DioError) {
+      print('DioError: ${e.response?.statusCode} ${e.response?.data}');
+    } else {
+      print('Error occurred: $e');
+    }
+    rethrow;
+  }
+}
