3fda4122bec94e103036988cd706f07d421740e9
jjjh02 <jinnyshur@korea.ac.kr>
2023-11-25 18:41:15 +0900

    navigation bar 상단 appbar 추가

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:inthon_frontend/pages/notice.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,8 +9,24 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.grey,
           elevation: 0,
+          toolbarHeight: 65,
+          leading: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // 다른 위젯을 우측으로 밀어내기 위해 Spacer 추가
+              IconButton(
+                icon: Icon(Icons.notifications_none_outlined),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Notice()),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
         body: Center(
           child: Text("home"),
