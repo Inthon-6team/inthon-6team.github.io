fd77623050cd7f749b39ad922282af10f9cc5317
jjjh02 <jinnyshur@korea.ac.kr>
2023-11-25 23:25:17 +0900

    login 페이지 구현

---
+++ lib/pages/login.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:inthon_frontend/components/image_data.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController _idcontroller = TextEditingController();
+  bool _isDisabled = true;
+
+  @override
+  void dispose() {
+    _idcontroller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.fromLTRB(60, 10, 60, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+                padding: EdgeInsets.fromLTRB(0, 150, 0, 0),
+                child: Text(
+                  'Welcome !',
+                  style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            Container(
+                padding: EdgeInsets.fromLTRB(0, 40, 0, 5),
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            SizedBox(
+              height: 0,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+              height: 50,
+              child: TextFormField(
+                controller: _idcontroller,
+                enabled: true,
+                style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                    letterSpacing: -0.50,
+                    color: _isDisabled ? Color(0xff9e9e9e) : Color(0xff212121)),
+                obscureText: false,
+                decoration: InputDecoration(
+                  hintText: 'ID',
+                  hintStyle: TextStyle(
+                    color: Color(0xff9e9e9e),
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18,
+                    letterSpacing: -0.50,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0), // 원하는 모양으로 조절
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0), // 원하는 모양으로 조절
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+              height: 50,
+              child: TextFormField(
+                controller: _idcontroller,
+                enabled: true,
+                style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                    letterSpacing: -0.50,
+                    color: _isDisabled ? Color(0xff9e9e9e) : Color(0xff212121)),
+                obscureText: false,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                    color: Color(0xff9e9e9e),
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18,
+                    letterSpacing: -0.50,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0), // 원하는 모양으로 조절
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0), // 원하는 모양으로 조절
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
+              child: Container(
+                width: 300,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Color(0xFFD9AE89),
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Center(
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Container(
+                width: 300,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Color(0xFFD9AE89),
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Row(children: [
+                  //ImageData(IconsPath.googlelogo, width: 20, height: 20),
+                  Text(
+                    'Sign In with Google',
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ]),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
